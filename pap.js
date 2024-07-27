b601dc5b930903eea7505f36d609599b9e26ebe9
lycanthrope8 <jubayerhossain9999@gmail.com>
2024-07-27 16:46:05 +0600

    policies are stored locally in PAP

--- pap.js
+++ pap.js
@@ -1,18 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+
 class PAP {
     constructor() {
-        this.policies = {};
+        this.policiesDir = path.join(__dirname, 'policies');
+
+        // Ensure the policies directory exists
+        if (!fs.existsSync(this.policiesDir)) {
+            fs.mkdirSync(this.policiesDir);
+        }
     }
 
+    // Save policy to a file
     addPolicy(policyId, policyXml) {
-        this.policies[policyId] = policyXml;
+        const filePath = path.join(this.policiesDir, `${policyId}.xml`);
+        fs.writeFileSync(filePath, policyXml, 'utf8');
     }
 
+    // Load policy from a file
     getPolicy(policyId) {
-        return this.policies[policyId];
+        const filePath = path.join(this.policiesDir, `${policyId}.xml`);
+        if (fs.existsSync(filePath)) {
+            return fs.readFileSync(filePath, 'utf8');
+        } else {
+            return null;
+        }
     }
 
+    // Load all policies
     getAllPolicies() {
-        return this.policies;
+        const files = fs.readdirSync(this.policiesDir);
+        const policies = {};
+        files.forEach(file => {
+            const policyId = path.basename(file, '.xml');
+            const policyXml = fs.readFileSync(path.join(this.policiesDir, file), 'utf8');
+            policies[policyId] = policyXml;
+        });
+        return policies;
     }
 }
 
