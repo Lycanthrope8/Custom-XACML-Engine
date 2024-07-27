747b4985aabe3067b310b3b7824f41054be96673
lycanthrope8 <jubayerhossain9999@gmail.com>
2024-07-27 16:58:34 +0600

    attributes are stored locally

--- pip.js
+++ pip.js
@@ -1,13 +1,38 @@
+const fs = require('fs');
+const path = require('path');
+
 class PIP {
     constructor() {
-        this.attributes = {};
+        this.attributesFile = path.join(__dirname, 'attributes.json');
+        this.attributes = this.loadAttributes();
+    }
+
+    // Load attributes from the JSON file
+    loadAttributes() {
+        if (fs.existsSync(this.attributesFile)) {
+            const data = fs.readFileSync(this.attributesFile, 'utf8');
+            return JSON.parse(data);
+        } else {
+            return {};
+        }
+    }
+
+    // Save attributes to the JSON file
+    saveAttributes() {
+        fs.writeFileSync(this.attributesFile, JSON.stringify(this.attributes, null, 2), 'utf8');
     }
 
     setAttribute(subjectId, attributeId, attributeValue) {
         if (!this.attributes[subjectId]) {
             this.attributes[subjectId] = {};
         }
-        this.attributes[subjectId][attributeId] = attributeValue;
+        // Check for duplicate entry
+        if (this.attributes[subjectId][attributeId] === undefined) {
+            this.attributes[subjectId][attributeId] = attributeValue;
+            this.saveAttributes();
+        } else {
+            console.log(`Attribute ${attributeId} for subject ${subjectId} already exists.`);
+        }
     }
 
     getAttribute(subjectId, attributeId) {
